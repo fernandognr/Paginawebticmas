8cc9ed11f8770bdb09fec44c95ea2769e4b872de
Fernando De Palma <fernandognrr@gmail.com>
2023-02-27 03:19:34 -0300

    Guardando el archivo con botones, redes sociales y full responsive

---
+++ Java/java-cv.js
@@ -0,0 +1,42 @@
+function Leermasdatosgenerales(){
+    var puntos=document.getElementById("datosgenerales");
+    var vermas=document.getElementById("dato1");
+    var boton=document.getElementById("boton");
+    if (puntos.style.display ==="none"){   
+        puntos.style.display = "inline";
+        vermas.style.display = "none";
+        boton.innerHTML = "Mostrar Datos Generales";
+    }else{
+        puntos.style.display = "none";
+        vermas.style.display = "inline";
+        boton.innerHTML = "Ocultar Datos Generales";
+    }
+}
+function Leermasformacionacademica(){
+    var puntos=document.getElementById("Formacionacademica");
+    var vermas=document.getElementById("dato2");
+    var boton=document.getElementById("boton2");
+    if (puntos.style.display ==="none"){   
+        puntos.style.display = "inline";
+        vermas.style.display = "none";
+        boton.innerHTML = "Mostrar Formacion Academica";
+    }else{
+        puntos.style.display = "none";
+        vermas.style.display = "inline";
+        boton.innerHTML = "Ocultar Formacion Academica";
+    }
+}
+function Leermasexperiencialaboral(){
+    var puntos=document.getElementById("Experiencialaboral");
+    var vermas=document.getElementById("dato3");
+    var boton=document.getElementById("boton3");
+    if (puntos.style.display ==="none"){   
+        puntos.style.display = "inline";
+        vermas.style.display = "none";
+        boton.innerHTML = "Mostrar Experiencia Laboral";
+    }else{
+        puntos.style.display = "none";
+        vermas.style.display = "inline";
+        boton.innerHTML = "Ocultar Experiencia Laboral";
+    }
+}
