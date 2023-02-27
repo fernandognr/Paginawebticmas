410bf0edd35318c4efd5d61b7186011dbaae2085
Fernando De Palma <fernandognrr@gmail.com>
2023-02-27 04:45:44 -0300

    Agregue comentarios al archivo java

--- Java/java-cv.js
+++ Java/java-cv.js
@@ -1,3 +1,4 @@
+//Muestra y oculta la pestaña de Datos Generales
 function Leermasdatosgenerales(){
     var puntos=document.getElementById("datosgenerales");
     var vermas=document.getElementById("dato1");
@@ -12,6 +13,7 @@ function Leermasdatosgenerales(){
         boton.innerHTML = "Ocultar Datos Generales";
     }
 }
+//Muestra y oculta la pestaña de Formacion Academica
 function Leermasformacionacademica(){
     var puntos=document.getElementById("Formacionacademica");
     var vermas=document.getElementById("dato2");
@@ -26,6 +28,7 @@ function Leermasformacionacademica(){
         boton.innerHTML = "Ocultar Formacion Academica";
     }
 }
+//Muestra y oculta la pestaña de Experiencia laboral 
 function Leermasexperiencialaboral(){
     var puntos=document.getElementById("Experiencialaboral");
     var vermas=document.getElementById("dato3");
